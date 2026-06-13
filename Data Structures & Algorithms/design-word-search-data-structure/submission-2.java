ac6339bdd3aa93b252a052bd1dc57a9126f60e99
Akshara Thotapalli <thotapalliakshara2@gmail.com>
2026-06-13 22:26:29 +0530

    Add: design-word-search-data-structure - submission-2

---
+++ Data Structures & Algorithms/design-word-search-data-structure/submission-2.java
@@ -0,0 +1,64 @@
+class WordDictionary {
+    WordDictionary[] child;
+    boolean flag;
+    public WordDictionary() {
+        child=new WordDictionary[26];
+        flag=false;
+    }
+
+    public void addWord(String word) {
+        WordDictionary temp=this;
+        for(int i=0;i<word.length();i++)
+        {
+            int c=word.charAt(i)-'a';
+            if(temp.child[c]==null)
+            {
+                temp.child[c]=new WordDictionary();
+            }
+            temp=temp.child[c];
+        }
+        temp.flag=true;
+    }
+   public boolean checkString(WordDictionary temp,String word,int i)
+   {
+    if(i==word.length())
+    {
+        return temp.flag;
+    }
+     char c=word.charAt(i);
+     if(c=='.')
+     {
+        for(int j=0;j<26;j++)
+        {
+          if(temp.child[j]!=null)
+          {
+            if(checkString(temp.child[j],word,i+1))
+            {
+                return true;
+            }
+          }
+        }
+     }
+     else{
+        if(temp.child[c-'a']==null)
+        {
+            return false;
+        }
+        else{
+            if(checkString(temp.child[c-'a'],word,i+1))
+            {
+                return true;
+            }
+        }
+     } 
+     return false; 
+   }
+    public boolean search(String word) {
+        WordDictionary temp=this;
+              if(checkString(temp,word,0))
+              {
+                return true;
+              }
+            return false;
+    }
+}
