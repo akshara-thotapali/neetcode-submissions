aed0429135349bd9b629b5bcb8425be74d144b23
Akshara Thotapalli <thotapalliakshara2@gmail.com>
2026-06-28 14:38:18 +0530

    Add: sum-of-two-integers - submission-3

---
+++ Data Structures & Algorithms/sum-of-two-integers/submission-3.java
@@ -0,0 +1,39 @@
+class Solution {
+    public int getSum(int a, int b) {
+        int c=0;
+        int ans=0;
+       for(int i=0;i<32;i++)
+       {
+        int bita=((a>>i)&1);
+        int bitb=((b>>i)&1);
+         if(bita==1 && bitb==1)
+         {
+            if(c==1)
+            {
+                ans|=(1<<i);
+            }
+                c=1;
+         }
+         else if(bita==1 || bitb==1)
+         {
+            if(c==1)
+            {
+                c=1;
+            }
+            else{
+                ans|=(1<<i);
+                c=0;
+            }
+         }
+         else
+         {
+            if(c==1)
+            {
+                ans|=(1<<(i));
+                c=0;
+            }
+         }
+       }
+       return ans;
+    }
+}
